d938ade832bdde03e42246a4f1011685a45f71e6
Abhiram <abhirambsn@gmail.com>
2023-10-15 16:06:11 +0530

    Added Executable Permissions added

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t kali-custom --build-arg gid=$(id -g) --build-arg uid=$(id -u) --build-arg user=abhiram-bhallamudi --build-arg group=abhiram-bhallamudi .
+docker build -t kali-custom --build-arg gid=$(id -g) --build-arg uid=$(id -u) --build-arg user=$1 --build-arg group=$1 .
