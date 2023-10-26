a8105f7ac610b2635804f8df791c643f774ea041
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2023-10-26 13:06:32 +0530

    Added automated setup.sh Script

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Kali Docker
 
----
-Kali Setup to run on any linux in a docker container
----
+Kali Linux to run on any OS in a Docker Container
+
+## Setup Instructions
+
+Follow the below instructions on your terminal to get setup with the tool.
+
+or you can use our readymade `setup.sh` script for the same
+
+### Automated Script
+```bash
+wget https://raw.githubusercontent.com/abhirambsn/setup.sh | bash
+```
+
+### Manual Way
+```bash
+git clone https://github.com/abhirambsn/kali-docker.git
+cd kali-docker/kalictl
+poetry install # Install script and dependencies
+
+kalictl init # Initialize config
+kalictl build # Build Images
+kalictl start # Start the compose stack
+kalictl exec /bin/bash # To Get into shell
+```
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+command_exists() {
+  if command -v "$1" &>/dev/null; then
+    echo "Command '$1' exists"
+  else
+    echo "Command '$1' does not exist, install $1 to continue"
+    exit 1
+  fi
+}
+
+# Check if required dependencies exist
+command_exists git
+coomand_exists poetry
+
+git_path=$(command -v git)
+poetry_path=$(command -v poetry)
+
+$git_path clone https://github.com/abhirambsn/kali-docker.git
+cd kali-docker/kalictl
+$poetry_path install # Install script and dependencies
+
+kalictl_path=$(command -v kalictl)
+
+$kalictl_path init # Initialize config
+$kalictl_path build # Build Images
+$kalictl_path start # Start the compose stack
+
+echo "[+] Execute 'kalictl exec /bin/bash' to enter the container's shell"
+echo "For more usage information, use 'kalictl [<command>] --help'"
