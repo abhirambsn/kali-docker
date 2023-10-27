c61342bcb0b51e351a196fc226aac3c245c0b775
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2023-10-27 18:57:18 +0530

    Setup Script added to include support for pipx

--- setup.sh
+++ setup.sh
@@ -14,14 +14,43 @@ command_exists() {
   fi
 }
 
+get_os() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "linux"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "macos"
+  elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "cygwin"
+  elif [[ "$OSTYPE" == "msys" ]]; then
+    echo "windows"
+  elif [[ "$OSTYPE" == "win32" ]]; then
+    echo "windows"
+  elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    echo "freebsd"
+  else
+    echo "unknown"
+  fi
+}
+
+is_pip = 0
+is_pip3 = 0
+is_pipx = 0
+
 # Check if required dependencies exist
 command_exists git
 command_exists poetry
 command_exists docker
-command_exists pip
-is_pip=$?
-command_exists pip3
-is_pip3=$?
+
+if [[ $(get_os) != "linux" ]]; then
+  command_exists pip
+  is_pip=$?
+  command_exists pip3
+  is_pip3=$?
+else
+  command_exists pipx
+  is_pipx=$?
+fi
+
 
 git_path=$(command -v git)
 poetry_path=$(command -v poetry)
@@ -32,14 +61,24 @@ $git_path clone https://github.com/abhirambsn/kali-docker.git $HOME/kali-docker
 cd kali-docker/kalictl
 $poetry_path install # Install script and dependencies
 $poetry_path build # Build Dependencies
-if $is_pip; then
-  $(command -v pip) install dist/*.whl
-elif $is_pip3; then
-  $(command -v pip3) install dist/*.whl
+if [[ $(get_os) -ne "linux" ]]; then
+  if $is_pip; then
+    $(command -v pip) install dist/*.whl
+  elif $is_pip3; then
+    $(command -v pip3) install dist/*.whl
+  else
+    echo "$RED[x] pip or pip3 not found, install either of them to continue$RESET"
+    exit 1
+  fi
 else
-  echo "$RED[x] pip or pip3 not found, install either of them to continue$RESET"
-  exit 1
+  if $is_pipx; then
+    $(command -v pipx) install dist/*.whl
+  else
+    echo "$RED[x] pipx not found, install it to continue$RESET"
+    exit 1
+  fi
 fi
+
 kalictl_path=$(command -v kalictl)
 
 # Create a docker network with name kali-net and subnet 10.0.0.0/16
